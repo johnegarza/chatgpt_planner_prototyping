31ed8462f81d742c3ab5d99bd8a710a901b446e5
John Garza <johnegarza@gmail.com>
2023-05-18 23:45:39 -0700

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    modified_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    tags = db.Column(db.String(120))
+    project = db.Column(db.String(120))
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    due_date = db.Column(db.DateTime, nullable=False)
+    priority = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    notes = db.relationship('Note', backref='task', lazy=True)
+    tags = db.Column(db.String(120))
+    project = db.Column(db.String(120))
+
+class Habit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    frequency = db.Column(db.Integer, nullable=False)
+    due_date = db.Column(db.DateTime, nullable=False)
+    type = db.Column(db.String(120), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
---
+++ app/views.py
@@ -0,0 +1,37 @@
+from flask import request, render_template, redirect, url_for
+from . import db
+from .models import Note
+
+@app.route('/notes', methods=['GET', 'POST'])
+def notes():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        tags = request.form['tags']
+        project = request.form['project']
+
+        new_note = Note(title=title, body=body, tags=tags, project=project)
+        db.session.add(new_note)
+        db.session.commit()
+
+        return redirect(url_for('notes'))
+
+    notes = Note.query.order_by(Note.created_at.desc()).all()
+    return render_template('notes.html', notes=notes)
+
+@app.route('/notes/<int:id>', methods=['GET', 'POST'])
+def edit_note(id):
+    note = Note.query.get_or_404(id)
+
+    if request.method == 'POST':
+        note.title = request.form['title']
+        note.body = request.form['body']
+        note.tags = request.form['tags']
+        note.project = request.form['project']
+        note.last_modified = datetime.now()
+        db.session.commit()
+
+        return redirect(url_for('notes'))
+
+    return render_template('edit_note.html', note=note)
+

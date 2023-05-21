7e4f2b2e59da95443c84e6b8acb94017fe5878e3
John Garza <johnegarza@gmail.com>
2023-05-21 02:55:44 -0700

    Manually fix some unneeded whitespace

--- app/views.py
+++ app/views.py
@@ -41,10 +41,10 @@ def edit_note(id):
 @views.route('/new_note', methods=['GET', 'POST'])
 def new_note():
     if request.method == 'POST':
-        title = request.form['title']  
-        body = request.form['body']  
+        title = request.form['title']
+        body = request.form['body']
         tag_names = request.form['tags'].split(',')
-        project = request.form['project']  
+        project = request.form['project']
 
         tags = []
         for tag_name in tag_names:
@@ -56,11 +56,11 @@ def new_note():
 
             tags.append(tag)
 
-        new_note = Note(title=title, body=body, tags=tags, project=project)  
+        new_note = Note(title=title, body=body, tags=tags, project=project)
 
-        db.session.add(new_note)  
-        db.session.commit()  
+        db.session.add(new_note)
+        db.session.commit()
 
-        return redirect(url_for('views.notes'))  
+        return redirect(url_for('views.notes'))
 
     return render_template('new_note.html')
